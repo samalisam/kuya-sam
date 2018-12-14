b7b50c083cb74c69d9e1406721fbd523786ac7b3
Sam Wymann <samalisam@hotmail.ch>
2018-12-14 12:30:01 +0100

    Undo

--- src/components/global/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-import TheToolbar from './TheToolbar.vue';
-import TheMainView from './TheMainView.vue';
-
-Vue.component('TheToolbar', TheToolbar);
-Vue.component('TheMainView', TheMainView);
