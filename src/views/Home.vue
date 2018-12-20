8e2c9038e680180bfdba61e8232ecdf5768dfa9c
Sam Wymann <samalisam@hotmail.ch>
2018-12-20 22:16:43 +0100

    Working on carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home">
     <KContainer>
-      <div class="inner-container">
-        <h1>I shoot photos</h1>
-        <p>But wait, that's not all.</p>
-        <KButton>Check out the full gallery</KButton>
+      <div class="content">
+        <div class="inner-content">
+          <h1>I shoot photos.</h1>
+          <p>But wait, that's not all.</p>
+          <KButton>Check out the full gallery</KButton>
+        </div>
       </div>
-      <div class="inner-gallery">
-        <div class="gallery-item">Test-Item</div>
+      <div class="gallery">
+        <div class="inner-gallery">
+          <div class="gallery-img">
+            <img src="../assets/Cool Blues.png">
+          </div>
+          <div class="gallery-img">
+            <img src="../assets/JShine.jpg">
+          </div>
+          <div class="gallery-img">
+            <img src="../assets/MegaTron.jpg">
+          </div>
+          <div class="gallery-img">
+            <img src="../assets/Piggy Pink.jpg">
+          </div>
+        </div>
+        <div class="gallery-indexer"></div>
       </div>
     </KContainer>
   </div>
