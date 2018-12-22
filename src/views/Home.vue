f02595f9111a6ec019010c602332f99177334795
Sam Wymann <samalisam@hotmail.ch>
2018-12-22 23:49:04 +0100

    Did some carousel stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,26 +9,83 @@
         </div>
       </div>
       <div class="gallery">
-        <div class="inner-gallery">
+        <div
+          ref="innerGallery"
+          class="inner-gallery"
+        >
           <div class="gallery-img">
-            <img src="../assets/Cool Blues.png">
+            <div class="img-placeholder">
+              <img src="../assets/Cool Blues.png">
+            </div>
           </div>
           <div class="gallery-img">
-            <img src="../assets/JShine.jpg">
+            <div class="img-placeholder">
+              <img src="../assets/JShine.jpg">
+            </div>
           </div>
           <div class="gallery-img">
-            <img src="../assets/MegaTron.jpg">
+            <div class="img-placeholder">
+              <img src="../assets/MegaTron.jpg">
+            </div>
           </div>
           <div class="gallery-img">
-            <img src="../assets/Piggy Pink.jpg">
+            <div class="img-placeholder">
+              <img src="../assets/Moonlit Asteroid.png">
+            </div>
+          </div>
+          <div class="gallery-img">
+            <div class="img-placeholder">
+              <img src="../assets/Piggy Pink.jpg">
+            </div>
           </div>
         </div>
-        <div class="gallery-indexer"></div>
+        <div class="gallery-indexer">
+          <div class="index-element"></div>
+          <div class="index-element"></div>
+          <div class="index-element"></div>
+          <div class="index-element"></div>
+          <div class="index-element"></div>
+        </div>
       </div>
     </KContainer>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  data() {
+    return {
+      galleryIndex: 0,
+      gallery: null
+    }
+  },
+  mounted() {
+    this.gallery = this.$refs.innerGallery
+    let interval = 6000
+    setInterval(() => {
+      if(this.gallery != null) {
+        let imagesInGallery = 4
 
-</style>
+        if(this.galleryIndex >= imagesInGallery) {
+          this.galleryIndex = 0
+        } else {
+          this.galleryIndex++
+        }
+
+        this.scroll(this.galleryIndex)
+      }
+    }, interval);
+  },
+  methods: {
+    scroll(imageIndex) {
+      let galleryWidth = this.gallery.clientWidth
+      let galleryPosition = galleryWidth * imageIndex
+
+      this.gallery.scrollTo({
+        left: galleryPosition,
+        behavior: 'smooth'
+      })
+    }
+  }
+}
+</script>
